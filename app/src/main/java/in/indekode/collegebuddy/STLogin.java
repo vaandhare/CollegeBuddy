8dcfe66876d87fbbab11229fe90f74d1e635d69c
Vaibhav Andhare <vaibhavandhare7@gmail.com>
2019-03-17 20:43:51 +0530

    Private Policy and Terms & Condition added.

--- app/src/main/java/in/indekode/collegebuddy/STLogin.java
+++ app/src/main/java/in/indekode/collegebuddy/STLogin.java
@@ -1,26 +1,90 @@
 package in.indekode.collegebuddy;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class STLogin extends AppCompatActivity {
 
     CardView bt_std, bt_tchr;
+    TextView privacy_policy, termsAC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stlogin);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         bt_std = findViewById(R.id.Login_to_std);
         bt_tchr = findViewById(R.id.Login_to_teacher);
+
+        privacy_policy = findViewById(R.id.Private_policy);
+        privacy_policy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(STLogin.this);
+                alert.setTitle("Privacy Policy");
+
+                WebView wv = new WebView(STLogin.this);
+                wv.loadUrl("http://indekode.ml/CollegeBuddy/PrivacyPolicy.html");
+                wv.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        view.loadUrl(url);
+
+                        return true;
+                    }
+                });
+
+                alert.setView(wv);
+                alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                alert.show();
+            }
+        });
+
+        termsAC = findViewById(R.id.TermsAndCondn);
+        termsAC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(STLogin.this);
+                alert.setTitle("Terms & Conditions");
+
+                WebView wv = new WebView(STLogin.this);
+                wv.loadUrl("http://indekode.ml/CollegeBuddy/terms_and_conditions.html");
+                wv.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        view.loadUrl(url);
+
+                        return true;
+                    }
+                });
+
+                alert.setView(wv);
+                alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+                alert.show();
+            }
+        });
 
         bt_std.setOnClickListener(new View.OnClickListener() {
             @Override
